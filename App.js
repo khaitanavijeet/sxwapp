30cd3d38f25f1fb30e1c95c95507518643f952c1
khaitanavijeet <khaitanavijeet@gmail.com>
2021-08-31 14:08:02 +0530

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { View, StyleSheet, Text } from 'react-native'
+import WebView from 'react-native-webview'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source = {{ uri:'http://sxwfeed.s3-website.ap-south-1.amazonaws.com/' }}
+      />
   );
 }
 
